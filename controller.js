0b11413bd15e44cc23279c840d94103d3e5f5437
Alexander <saneknv05@gmail.com>
2023-12-24 15:31:58 +0300

    a

--- controller.js
+++ controller.js
@@ -3,7 +3,7 @@ const db = require('./db.js');
 class Controller {
     async create(req, res){
         const {name, title, price, img} = req.body;
-        const newItem = await db.query(`INSERT INTO cars (name, title, price, img) values ($1, $2, $3, $4) RETURNING *`, [name, title, price, img]);
+        const newItem = await db.query(`INSERT INTO cooldatabase (name, title, price, img) values ($1, $2, $3, $4) RETURNING *`, [name, title, price, img]);
         res.json(newItem.rows[0]);
     }
 }
